c09ce139c2ada4a8b3bbc3a6f6423b02eddf9e2a
Samuel <samuel310597@gmail.com>
2020-06-23 21:41:48 +0200

    show battery level on hover

--- components/Battery.tsx
+++ components/Battery.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 import BatteryLevel from './BatteryLevel';
 import Emoji from './Emoji'
 
-export default class Battery extends React.Component<{nb: number, batteryState: number}> {
+export default class Battery extends React.Component<{nb: number, batteryState: number}, {isTouch: number}> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+          isTouch: 0
+        };
+    }
+    
+    
 	render() {
 		return (
-			<View style={[styles.container]}>
+			<View style={[styles.container]} onTouchStart={() => this.setState({isTouch : 1})} onTouchEnd={() => this.setState({isTouch : 0})}>
 				<View style={[styles.topBattery]}></View>
 				<View style={[styles.battery]}>
                     <BatteryLevel nb={this.props.nb}></BatteryLevel>
-                    {this.props.batteryState==2 ? <Feather name="battery-charging" size={70} color="black"/> : <View></View>}
+                    {(this.props.batteryState==2 && this.state.isTouch == 0)? <Feather name="battery-charging" size={70} color="black"/> : <View></View>}
+                    {this.state.isTouch == 1 ? <Text style={[styles.nbBattery]}>{this.props.nb}</Text>: <View></View>}
                 </View>
 			</View>
 		);
@@ -41,5 +50,9 @@ const styles = StyleSheet.create({
     },
     container: {
         alignItems: 'center'
+    },
+    nbBattery:{
+        fontSize: 60,
+        marginBottom: 8
     }
 });
--- components/BatteryLevel.tsx
+++ components/BatteryLevel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class BatteryLevel extends React.Component<{ nb: number }> {
